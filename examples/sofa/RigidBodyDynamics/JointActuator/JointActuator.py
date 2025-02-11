385926c0ca5d4a12fbe4eb558c6645a019c01517
Olivier Roussel <olivier.roussel@inria.fr>
2025-02-11 11:16:33 +0100

    Fix joint actuator example

--- examples/sofa/RigidBodyDynamics/JointActuator/JointActuator.py
+++ examples/sofa/RigidBodyDynamics/JointActuator/JointActuator.py
@@ -17,7 +17,6 @@ def createScene(rootNode):
     rootNode.addObject('RequiredPlugin', name='SoftRobots')
     rootNode.addObject('RequiredPlugin', name='SoftRobots.Inverse')
     rootNode.addObject('RequiredPlugin', name='Sofa.RigidBodyDynamics')
-    # rootNode.addObject('RequiredPlugin', name='ArticulatedSystemPlugin')
     rootNode.addObject('RequiredPlugin', name='Sofa.Component.AnimationLoop')  # Needed to use components [FreeMotionAnimationLoop]
     rootNode.addObject('RequiredPlugin', name='Sofa.Component.Constraint.Lagrangian.Correction')  # Needed to use components [UncoupledConstraintCorrection]
     rootNode.addObject('RequiredPlugin', name='Sofa.Component.LinearSolver.Iterative')  # Needed to use components [CGLinearSolver]
@@ -29,9 +28,9 @@ def createScene(rootNode):
     rootNode.addObject('RequiredPlugin', name='Sofa.Component.Engine.Generate')  # Needed to use components [GenerateRigidMass]
     rootNode.addObject('RequiredPlugin', name='Sofa.Component.IO.Mesh')  # Needed to use components [MeshSTLLoader]
     rootNode.addObject('RequiredPlugin', name='Sofa.Component.Mapping.NonLinear')  # Needed to use components [RigidMapping]
-    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Mass')  # Needed to use components [UniformMass]
     rootNode.addObject('RequiredPlugin', name='Sofa.Component.Topology.Container.Constant')  # Needed to use components [MeshTopology]
     rootNode.addObject('RequiredPlugin', name='Sofa.GL.Component.Rendering3D')  # Needed to use components [OglModel]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Collision.Geometry')
 
     rootNode.dt = 0.1
     rootNode.gravity = [0., 0., 0.]
@@ -39,20 +38,20 @@ def createScene(rootNode):
     # rootNode.addObject('VisualStyle', displayFlags='showBehaviorModels')
     rootNode.addObject('BackgroundSetting', color=[0, 0.168627, 0.211765, 1.])
     rootNode.addObject('FreeMotionAnimationLoop')
-    rootNode.addObject('QPInverseProblemSolver', epsilon=0)
+    # rootNode.addObject('QPInverseProblemSolver', epsilon=0)
+    rootNode.addObject('QPInverseProblemSolver', printLog=True, epsilon=1.e-15, allowSliding=False, actuatorsOnly=True,
+                       maxIterations=5000, tolerance=1e-15,
+                       qpSolver="proxQP")
+                      #  qpSolver="qpOASES")
 
     # Target position of the end effector
     goal = rootNode.addChild('Goal')
     goal.addObject('EulerImplicitSolver', firstOrder=True)
     goal.addObject('CGLinearSolver', iterations=100, tolerance=1e-5, threshold=1e-5)
-    goal.addObject('MechanicalObject', name='goalEE', position=[0.0, 0.05, 0], # 5cm for initial goal EE position + 4.5cm for offsetting the robot
+    goal.addObject('MechanicalObject', name='goalEE', position=[0.05, 0.05, 0], # 5cm for initial goal EE position + 4.5cm for offsetting the robot
                    showObject=True, showObjectScale=0.005, drawMode=2)
     goal.addObject('UncoupledConstraintCorrection')
-    goal.addObject('SphereCollisionModel', radius=0.006)
-
-    # goalContact = goal.addChild('contact')
-    # goalContact.addObject('MechanicalObject')
-    # goalContact.addObject('RigidMapping',template='Vec3d,Vec3d', input="@..", index=0)
+    goal.addObject('SphereCollisionModel', radius=0.006) # for mouse moving
 
     # Simulation node
     simulation = rootNode.addChild('Simulation')
@@ -69,7 +68,6 @@ def createScene(rootNode):
     robotNode.addObject('JointActuator', template='Vec1', index=0,
                      maxAngle=2.5, minAngle=-2.5, maxAngleVariation=0.005)
 
-
     framesNode = robotNode.getChild('Frames')
     toolFrameNode = framesNode.getChild('tool0')
     toolFrameNode.getObject('dof').showObject = True
@@ -81,28 +79,5 @@ def createScene(rootNode):
     effector.addObject('PositionEffector', indices=0, 
                        effectorGoal=goal.goalEE.findData('position').getLinkPath(),
                        useDirections=[1, 1, 0])
-
-    # visual = rigid.addChild('VisualModel')
-    # visual.addObject('MeshSTLLoader', name='loader', filename=dirPath + 'mesh/arm.stl', translation=[-4.5, 0., 0.],
-    #                  scale3d=[0.1, 0.1, 0.1])
-    # visual.addObject('MeshTopology', src='@loader')
-    # visual.addObject('OglModel', color=[0.9, 0.9, 0.9, 1.])
-    # visual.addObject('RigidMapping', index=1)
-
-    # rigid.addObject('GenerateRigidMass', name='mass', density=0.002, src=visual.loader.getLinkPath())
-    # rigid.addObject('FixedProjectiveConstraint', template='Rigid3', indices=0)
-    # rigid.addObject('ArticulatedSystemMapping', input1=object.dofs.getLinkPath(), output=rigid.dofs.getLinkPath())
-    # rigid.addObject('UniformMass', vertexMass=rigid.mass.findData('rigidMass').getLinkPath())
-
-    # object.addObject('UncoupledConstraintCorrection')
-    # object.addObject('ArticulatedHierarchyContainer')
-
-    # articulationCenters = object.addChild('Articulation')
-    # articulationCenter = articulationCenters.addChild('ArticulationCenter')
-    # articulationCenter.addObject('ArticulationCenter', parentIndex=0, childIndex=1, posOnParent=[0., 0., 0.],
-    #                              posOnChild=[-4.5, 0., 0.], articulationProcess=0)
-    # articulation = articulationCenter.addChild('Articulations')
-    # articulation.addObject('Articulation', translation=False, rotation=True, rotationAxis=[0, 0, 1],
-    #                        articulationIndex=0)
 
     return rootNode
